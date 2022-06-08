1f6a3cc5487677ea47222ba5c5fb77fdba83a3bf
StefanYosifov <stefyosifov@gmail.com>
2022-06-08 16:18:14 +0300

    Implementing List<int>

---
+++ Implementing Queue and Stack/Implementing Queue and Stack/MyList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementing_Queue_and_Stack
+{
+    public class MyList
+    {
+
+        public MyList(int capacity)
+        {
+            this.capacity = capacity;
+            this.data=new int[capacity];
+        }
+
+        private int[] data;
+        private int capacity;
+
+        public int Count { get;private set; }
+
+        public void Add(int value)
+        {
+            if(this.data.Length ==capacity)
+            {
+
+            }
+
+            data[this.capacity] = value;
+            this.Count++;
+        }
+
+        private void Resize()
+        {
+            var newCapacity=this.data.Length*2;
+            var newData=new int[newCapacity];
+            for(int i = 0; i < this.data.Length; i++)
+            {
+                newData[i]=this.data[i];
+            }
+            this.data=newData;
+        }
+
+
+    }
+}
