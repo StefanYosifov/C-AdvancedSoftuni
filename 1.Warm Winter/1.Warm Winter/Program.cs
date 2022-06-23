ed257c756bf2d33c321906d07581da4e3df2a454
StefanYosifov <stefyosifov@gmail.com>
2022-06-23 16:56:48 +0300

    Exercise before the final exam

---
+++ 1.Warm Winter/1.Warm Winter/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.Warm_Winter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] secondArr= Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> scarfs=new Queue<int>(secondArr);
+            Stack<int> hats=new Stack<int>(firstArr);
+            List<int> clothes = new List<int>();
+
+
+            while(scarfs.Count>0 && hats.Count > 0)
+            {
+                int peekHats = hats.Peek();
+                int peekScarfs = scarfs.Peek();
+
+                int total = peekScarfs + peekHats;
+
+
+                if (peekHats == peekScarfs)
+                {
+                    scarfs.Dequeue();
+                    int takeLastHat = hats.Peek() + 1;
+                    hats.Pop();
+                    hats.Push(takeLastHat);
+                }
+                else if (peekHats > peekScarfs)
+                {
+                    clothes.Add(hats.Pop() + scarfs.Dequeue());
+                }
+                else if(peekScarfs> peekHats)
+                {
+                    hats.Pop();
+                    
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine($"The most expensive set is: {clothes.Max()}");
+            Console.WriteLine(String.Join(" ",clothes));
+        }
+    }
+}
---
+++ 1.Warm Winter/Skeleton/Cocktail.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            collection = new List<Ingredient>();
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+        }
+
+        private List<Ingredient> collection { get; set; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int MaxAlcoholLevel { get; set; }
+
+        public int CurrentAlcoholLevel =>collection.Sum(x=>x.Alcohol);
+        public void Add(Ingredient ingredient)
+        {
+            if (this.collection.Count < this.Capacity && this.MaxAlcoholLevel > ingredient.Alcohol &&
+                this.collection.Any(x => x.Name == ingredient.Name) == false)
+            {
+                this.collection.Add(ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var findCocktail = collection.FirstOrDefault(x => x.Name == name);
+            return collection.Remove(findCocktail);
+           
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            var findCockTailIngredient=collection.FirstOrDefault(x => x.Name == name);
+            return findCockTailIngredient;
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return collection.OrderByDescending(x => x.Alcohol).FirstOrDefault();
+        }
+        
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+            foreach(var ingredient in collection)
+            {
+                sb.AppendLine(ingredient.ToString());
+            }
+           return sb.ToString().Trim();
+        }
+
+    }
+}
