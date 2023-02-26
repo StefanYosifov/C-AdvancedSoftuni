a8417c4c39f8aff658faf12a0c860c451a21fb24
StefanYosifov <stefyosifov@gmail.com>
2023-02-26 20:31:17 +0200

    Implemented my own list, a few things still remain

---
+++ Creating a List/Creating a List/MyList.cs
@@ -0,0 +1,221 @@
+﻿namespace Creating_a_List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class MyList<T> : IList<T>
+    {
+
+        private const int InitialCapacity = 4;
+        private T[] elements;
+        private int size;
+        private int capacity;
+
+
+        public MyList()
+        {
+            this.elements = new T[InitialCapacity];
+            this.capacity = InitialCapacity;
+            this.size = 0;
+        }
+
+        public T this[int index] 
+        { 
+            get 
+            {
+                if (ValidateIndex(index) == true) 
+                { 
+                    return elements[index];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            set 
+            {
+                if (ValidateIndex(index) == true)
+                {
+                    elements[index] = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            } 
+        
+        }
+           
+        
+
+        public int Count => size;
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T element)
+        {
+            if (this.Count == this.capacity)
+            {
+                this.elements = this.DoubleCapacity();
+            }
+
+
+            this.elements[size]=element;
+            this.size++;
+        }
+
+        public void Clear()
+        {
+            this.capacity = InitialCapacity;
+            this.size = 0;
+            this.elements = new T[InitialCapacity];
+        }
+
+        public bool Contains(T item)
+        {
+            for(int i = 0; i < this.elements.Length; i++)
+            {
+                T currentElement=this.elements[i];
+                if(object.Equals(currentElement, item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = 0; i < this.Count; i++)
+            {
+                yield return this.elements[i];
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            
+            for(int i = 0; i < this.size; i++)
+            {
+                T currentElement = this.elements[i];
+                if (object.Equals(currentElement, item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+
+        public void Insert(int index, T item)
+        {
+            if (index > this.size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            this.size++;
+            ShiftRight(index);
+            this.elements[index] = item;
+        }
+
+        private void ShiftRight(int index)
+        {
+            if (this.Count == this.capacity)
+            {
+                this.elements = this.DoubleCapacity();
+            }
+
+
+            for (int i=this.size; i > index; i--)
+            {
+                this.elements[i] = this.elements[i - 1];
+            }
+        }
+
+        // 2 5 7 9
+        // 2 1 5 7 9
+
+
+        public bool Remove(T item)
+        {
+            for(int i = 0; i < this.Count; i++)
+            {
+                T currentElement = this.elements[i];
+                if (object.Equals(currentElement, item))
+                {
+                    ShiftLeftElements(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void ShiftLeftElements(int index)
+        {
+            for(int i = index; i < this.Count-1; i++)
+            {
+                this.elements[i] = this.elements[i+1];
+            }
+            this.size--;
+        }
+
+
+        // 1 2 3 4 5 6 3 4 5 6
+        // 1 2 4 5 6 3 4 5 6
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeftElements(index);
+        }
+
+        // 1 2 4 7 9 -> 4
+        // 1 2 7 9
+
+        private T[] DoubleCapacity()
+        {
+            T[] newArray=new T[this.capacity*2];
+            for(int i = 0; i < this.Count; i++)
+            {
+                newArray[i] = this.elements[i];
+            }
+            this.capacity *= 2;
+            return newArray;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private T[] HalveCapacity()
+        {
+            T[] newArr = new T[this.capacity/2];
+            for(int i = 0; i < this.Count; i++)
+            {
+                newArr[i] = this.elements[i];
+            }
+            return newArr;
+        }
+
+        private bool ValidateIndex(int index)
+        {
+            if(index>this.capacity || index < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Creating a List/Creating a List/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Creating_a_List
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<int> list=new List<int>();
+
+            MyList<int> myList = new MyList<int>();
+
+
+            list.Remove(1);
+
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+
+            myList.Remove(3);
+            myList.Clear();
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            myList.Add(10);
+            myList.Add(15);
+            myList.Insert(0, 5);
+            myList.Insert(2, 1);
+            myList.Insert(4, 2);
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("==========");
+            myList.RemoveAt(3);
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
